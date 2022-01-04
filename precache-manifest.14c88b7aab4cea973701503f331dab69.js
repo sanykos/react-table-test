a692c3f7ffdee06196fa2bb8ebd20d13cda11094
Alexander Kosykh <sanykos446@gmail.com>
2022-01-05 02:59:29 +0300

    Updates

---
+++ precache-manifest.14c88b7aab4cea973701503f331dab69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "249b4cd5178e2c2c2cc682eb1aaab03d",
+    "url": "/react-table-test/index.html"
+  },
+  {
+    "revision": "875b65914d7aaf9ec0e4",
+    "url": "/react-table-test/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e641ee866979d89da340",
+    "url": "/react-table-test/static/css/main.4c0d6917.chunk.css"
+  },
+  {
+    "revision": "875b65914d7aaf9ec0e4",
+    "url": "/react-table-test/static/js/2.80510f1a.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/react-table-test/static/js/2.80510f1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e641ee866979d89da340",
+    "url": "/react-table-test/static/js/main.f967315b.chunk.js"
+  },
+  {
+    "revision": "8b1d454652820d72dcef",
+    "url": "/react-table-test/static/js/runtime-main.c8171d6c.js"
+  }
+]);
